351a06ac8a1238c32ecd613c56999cf76d145a98
1tmc <158778648+1tmc@users.noreply.github.com>
2025-06-18 23:55:55 +0000

    Remove unused files and clean up code

--- src/components/events/events-slider/index.tsx
+++ src/components/events/events-slider/index.tsx
@@ -1,7 +1,7 @@
 import Breadcrumb from "@/components/common/Breadcrumb"
 import FooterOne from "@/layout/footers/FooterOne"
 import HeaderOne from "@/layout/headers/HeaderOne"
-import Brand from "@/components/common/Brand"
+
 import EventSliderArea from "./EventSliderArea"
 
 const EventSlider = () => {
@@ -11,7 +11,7 @@ const EventSlider = () => {
          <main>
             <Breadcrumb page_title="Our Events Slider" page_list="Event" style={false} />
             <EventSliderArea/>
-            <Brand style={true} />
+
          </main>
          <FooterOne />
       </>
